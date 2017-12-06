282ce4ac64955dcfa84fbfcf7d025e5414f0c6a1
Ryan Mack <ryanjmack21@gmail.com>
2017-12-06 12:00:52 -0500

    Add reset function and some comments

--- src/js/master.js
+++ src/js/master.js
@@ -43,6 +43,18 @@ function timer(seconds) {
   }, 1000);
 }
 
+// resets the timer in its entirety
+function reset() {
+  e.preventDefault();
+  clearInterval(countdown);
+  isTimerRunning = false;
+  isBreak = false;
+
+  sessionLength.innerText = '25:00';
+  breakLength.innerText = '5:00';
+  time.innerText = '25:00';
+}
+
 
 /***********************************************************************
 * View functions
@@ -62,12 +74,18 @@ function padTime(minutes, seconds) {
 
 // gets called when user wants to modify break or session length
 // takes in the DOM element and a boolean indicating whether user wants to add or sub time
+// only affects the timer IF the timer is NOT running
 function adjustTime(element, isIncrementing) {
+  // get the length of time and modify it
   let length = element.innerText.split(':');
   length[0] = parseInt(length[0]) + (isIncrementing ? 1 : -1);
+
+  // join the time and update the view
   length = length.join(":");
   element.innerText = length;
 
+  // if the timer isn't running change the timer's time
+  // else the timer will not be affected
   if (!isTimerRunning) {
     if (isBreak && element === breakLength) {
       time.innerText = length;
@@ -82,21 +100,27 @@ function adjustTime(element, isIncrementing) {
 * Controller funtions
 ***********************************************************************/
 function handleEvent(e) {
+  // there is a keydown event listener. Ignore all keys except for space bar
   if (e.keyCode && e.keyCode !== 32) {
     return;
   }
+  // clear the interval if the timer is running
   else if (isTimerRunning) {
     clearInterval(countdown);
   }
   else {
+    // get the time remaining, in seconds and pass it to the timer function to start it
     let seconds = time.innerText.trim().split(':')
          .reduce((a, b, i) => a += (i === 0) ? parseInt(b, 10) * 60 : parseInt(b, 10), 0);
     timer(seconds);
   }
+  // update flag
   isTimerRunning = !isTimerRunning;
 }
 
 
+// updates the break/session length.
+// If true is passed to adjustTime user is incrementing, else user is decrementing
 function handleControls(e) {
   const classList = e.target.classList.value;
 
@@ -120,3 +144,4 @@ function handleControls(e) {
 window.addEventListener('keydown', handleEvent);
 time.addEventListener('click', handleEvent);
 controls.addEventListener('click', handleControls);
+document.querySelector('.reset').addEventListener('click', reset);
